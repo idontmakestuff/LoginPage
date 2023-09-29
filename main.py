93993bfc7f5f50a30ebdb131488359af9697bbb9
idontmakestuff <mwthrowaway1@outlook.com>
2023-09-29 10:29:53 -0500

    Questions 1 and 2, quiz manager

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
+from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.app import App
 
-class LoginPageApp(App):
-    pass
+
+Builder.load_file("QuizPage.kv")
+class QuizPageApp(App):
+    def build(self):
+        return QuizManager()
 
 
-class LoginManager(ScreenManager):
+class QuizManager(ScreenManager):
     pass
 
 class Question1Screen(Screen):
-    pass
+    def answer_question(self, bool):
+        if bool:
+            self.manager.current = "correct"
+        else:
+            self.manager.current = "error"
 
+class Question2Screen(Screen):
+    def answer_question(self, answer):
+        if answer=="purple":
+            self.manager.current = "correct"
+        else:
+            self.manager.current = "error"
 class CorrectScreen(Screen):
-    pass
+    def next_question(self):
+        self.manager.current = "question2"
 
 class ErrorScreen(Screen):
-    pass
+    def next_question(self):
+        self.manager.current = "question2"
 
-LoginPageApp().run()
+
+
+QuizPageApp().run()
