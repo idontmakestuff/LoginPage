97a557dcbcf7540bd4751f5da88a0694106907cc
idontmakestuff <mwthrowaway1@outlook.com>
2023-10-03 10:32:25 -0500

    loginpage, loginscreen added

--- main.py
+++ main.py
@@ -1,38 +1,45 @@
+
+#
+# class Question1Screen(Screen):
+#     def answer_question(self, bool):
+#         if bool:
+#             self.manager.current = "correct"
+#         else:
+#             self.manager.current = "error"
+#
+# class Question2Screen(Screen):
+#     def answer_question(self, answer):
+#         if answer.lower()=="purple":
+#             self.manager.current = "correct"
+#         else:
+#             self.ids.invalid_guess.text = "Invalid guess. \n Try again!"
+#             self.ids.invalid_guess.color = "red"
+# class CorrectScreen(Screen):
+#     def next_question(self):
+#         self.manager.current = "question2"
+#
+# class ErrorScreen(Screen):
+#     def next_question(self):
+#         self.manager.current = "question2"
+#
+#
+#
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.app import App
-
-
-Builder.load_file("QuizPage.kv")
-class QuizPageApp(App):
+#
+#
+Builder.load_file("LoginPage.kv")
+class LoginPageApp(App):
     def build(self):
-        return QuizManager()
-
-
-class QuizManager(ScreenManager):
+        return LoginManager()
+#
+#
+class LoginManager(ScreenManager):
     pass
 
-class Question1Screen(Screen):
-    def answer_question(self, bool):
-        if bool:
-            self.manager.current = "correct"
-        else:
-            self.manager.current = "error"
 
-class Question2Screen(Screen):
-    def answer_question(self, answer):
-        if answer=="purple":
-            self.manager.current = "correct"
-        else:
-            self.manager.current = "error"
-class CorrectScreen(Screen):
-    def next_question(self):
-        self.manager.current = "question2"
+class LoginScreen(Screen):
+    pass
 
-class ErrorScreen(Screen):
-    def next_question(self):
-        self.manager.current = "question2"
-
-
-
-QuizPageApp().run()
+LoginPageApp().run()
