47cd44a16130ef59fd754b37c469a5213c1e75bf
idontmakestuff <mwthrowaway1@outlook.com>
2023-10-10 09:57:15 -0500

    this is NOT working

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ from kivy.app import App
 #
 Builder.load_file("LoginPage.kv")
 
-users = {"MrGreen":"$$$$$$"}
+
 class LoginPageApp(App):
     def build(self):
         return LoginManager()
@@ -53,10 +53,29 @@ class LoginScreen(Screen):
                 self.ids.notif.text="Incorrect Password!"
         else:
             self.ids.notif.text = "User does not exist"
-    def signup(self):
+    def gosignup(self):
         self.manager.current="signup"
+
 class SignupScreen(Screen):
-    pass
+
+    def signup(self,newuser,newpass,newpass2):
+        global users
+        if newuser in users:
+            self.ids.sign_notif.text = "Username taken"
+        else:
+            if caps in newpass:
+                if lowercase in newpass:
+                    if nums in newpass:
+                        if special_chars in newpass:
+                            if len(newpass)>= 8:
+                                if newpass==newpass2:
+
+                                    users[newuser]=newpass
+caps= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+lowercase=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+nums=["0","1","2","3","4","5","6","7","8","9"]
+special_chars=["!","@","#","$","%","^","&","*","(",")","_","-","+","=","[","]","{","}",".",","]
+users = {"MrGreen":"$$$$$$"}
 LoginPageApp().run()
 
 print("AAAAAAA")
